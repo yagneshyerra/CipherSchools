6550d74e1a471665bb9cd7e764f5c991c327a66f
yagneshyerra <122748437+yagneshyerra@users.noreply.github.com>
2023-06-25 16:14:24 +0530

    Add files via upload

---
+++ lecture3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+   // Declare 2 variable and then assign them values of 3,5 and then add them
+    // int a,b;
+    int a;
+    int b;
+
+    a = 3;
+    b = 5;
+    int c;
+    c = a + b;
+    cout<<c;
+
+    // cout<<a+b;
+    cout<<"a+b";
+    cout<<endl;
+    cout<<a+b<<endl<<"a+b";
+
+
+}
